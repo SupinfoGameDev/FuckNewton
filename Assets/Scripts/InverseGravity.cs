f39f6a6ee043c7551972a86b5dcf2bbf1b65aae9
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-04-30 22:18:44 +0200

    Update InverseGravity.cs

--- Assets/Scripts/InverseGravity.cs
+++ Assets/Scripts/InverseGravity.cs
@@ -1,36 +1,42 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class InverseGravity : MonoBehaviour {
-
-	int speed = 10;
-	// Use this for initialization
-	void Start () {
-	}
-	// Update is called once per frame
-	void Update(){
-		if(Input.GetButtonDown("Fire1"))
-		{
-			if(speed > 0)
-			{
-				Physics.gravity = new Vector3(0, speed, 0);
-				speed = -10;
-				moveAll();
-			}
-			else
-			{   
-				Physics.gravity = new Vector3(0, speed, 0);
-				speed = 10;
-				moveAll();
-			}
-		}
-	}
+public class InverseGravity : MonoBehaviour
+{
+    private int speed = 10;
 	
-	void moveAll()
-	{    
-		var objectsToEffect = GameObject.FindGameObjectsWithTag ("ObjectToEffect");
-		foreach (var obj in objectsToEffect) {
-			obj.Physics.gravity = Vector3(0, speed, 0);
-		}
-	}
+   // Use this for initialization
+    void Start ()
+    {
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (speed > 0)
+            {
+                Physics.gravity = new Vector3(0, speed, 0);
+                speed = -10;
+                this.moveAll();
+            }
+            else
+            {   
+                Physics.gravity = new Vector3(0, speed, 0);
+                speed = 10;
+                this.moveAll();
+            }
+        }
+    }
+
+    void moveAll()
+    {
+        var objectsToEffect = GameObject.FindGameObjectsWithTag("ObjectToEffect");
+        foreach (var obj in objectsToEffect)
+        {
+            // Il me semble que ce code ne compile pas : obj.Physics n'existe pas
+            //obj.Physics.gravity = new Vector3(0, speed, 0);
+        }
+    }
 }
