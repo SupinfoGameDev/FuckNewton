5a4b532ef1f14fdf6e97afdf4b40254487d6c7a5
mbouquerod <maxime.bouquerod@gmail.com>
2014-05-09 10:39:56 +0200

    Modification du InverseGravity.cs. Fonctionnelle sauf une fois la gavité vers le haut le personnage marche à l'envers.

--- Assets/Scripts/InverseGravity.cs
+++ Assets/Scripts/InverseGravity.cs
@@ -1,42 +1,28 @@
 using UnityEngine;
 using System.Collections;
 
-public class InverseGravity : MonoBehaviour
-{
-    private int speed = 10;
+public class InverseGravity : MonoBehaviour {
 	
-    // Use this for initialization
-    void Start()
-    {
-    }
-	
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            if (speed > 0)
-            {
-                Physics.gravity = new Vector3(0, speed, 0);
-                speed = -10;
-                this.MoveAll();
-            }
-            else
-            {   
-                Physics.gravity = new Vector3(0, speed, 0);
-                speed = 10;
-                this.MoveAll();
-            }
-        }
-    }
-
-    private void MoveAll()
-    {
-        var objectsToEffect = GameObject.FindGameObjectsWithTag("ObjectToEffect");
-        foreach (var obj in objectsToEffect)
-        {
-            // Il me semble que ce code ne compile pas : obj.Physics n'existe pas
-            //obj.Physics.gravity = new Vector3(0, speed, 0);
-        }
-    }
+	// Use this for initialization
+	void Update ()
+	{
+		if(Input.GetButtonDown("Fire2"))
+		{
+			Physics2D.gravity *= -1;
+			Quaternion rotation = transform.localRotation;
+			if (Physics2D.gravity.y > 0)
+			{
+				rotation.x = 180;
+				rotation.y = 0;
+				rotation.z = 0;
+			}
+			else
+			{
+				rotation.x = 0;
+				rotation.y = 0;
+				rotation.z = 0;
+			}
+			transform.localRotation = rotation;
+		}
+	}
 }
